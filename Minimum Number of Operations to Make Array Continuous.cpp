24ca98363cc9d77ba46b6693b672a25d57124e47
Aman Shankar singh <96378145+singhAman05@users.noreply.github.com>
2024-02-02 23:55:34 +0530

    Create Minimum Number of Operations to Make Array Continuous.cpp

---
+++ Minimum Number of Operations to Make Array Continuous.cpp
@@ -0,0 +1,34 @@
+/*
+  In this question we will sort the array and find the in range number and just subtract it with out_range
+*/
+
+
+
+class Solution {
+public:
+    int minOperations(vector<int>& nums) {
+        int n = nums.size();
+        
+        set<int> st(begin(nums), end(nums));
+        
+        vector<int> temp(begin(st), end(st));
+        
+        int result = INT_MAX;
+        
+        for(int i = 0; i<temp.size(); i++) {
+            
+            int left_range  = temp[i];
+            int right_range = left_range + n - 1;
+            
+            int j = upper_bound(begin(temp), end(temp), right_range) - begin(temp);
+            
+            int in_range  = j - i;
+            int out_range = n - in_range;
+            
+            result = min(result, out_range);
+            
+        }
+        
+        return result;
+    }
+};
