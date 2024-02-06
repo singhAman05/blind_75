25d4126ed6b78654b2399ae3b21934c6164d3119
Aman Shankar singh <96378145+singhAman05@users.noreply.github.com>
2024-02-06 17:02:05 +0530

    Create Widest Vertical Area Between Two Points Containing No Points.cpp

---
+++ Widest Vertical Area Between Two Points Containing No Points.cpp
@@ -0,0 +1,16 @@
+/*
+  In this question we will simply calculate the diff bw consecutive X-AXIS and return the max
+*/
+
+class Solution {
+public:
+    int maxWidthOfVerticalArea(vector<vector<int>>& points) {
+        sort(points.begin(),points.end());
+        int n = points.size();
+        int ans = 0;
+        for(int i=1;i<n;i++){
+            ans = max(ans,points[i][0]-points[i-1][0]);
+        }
+        return ans;
+    }
+};
