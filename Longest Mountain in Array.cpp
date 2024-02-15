11af73ef5d95845bcc4088e2e4523a4e7a754f39
Aman Shankar singh <96378145+singhAman05@users.noreply.github.com>
2024-02-15 23:50:31 +0530

    Update Longest Mountain in Array.cpp

--- Longest Mountain in Array.cpp
+++ Longest Mountain in Array.cpp
@@ -19,3 +19,19 @@ public:
         return ans>=3?ans:0;
     }
 };
+
+
+//TC O(N)
+class Solution {
+public:
+        int longestMountain(vector<int> A) {
+        int N = A.size(), res = 0;
+        vector<int> up(N, 0), down(N, 0);
+        for (int i = N - 2; i >= 0; --i) if (A[i] > A[i + 1]) down[i] = down[i + 1] + 1;
+        for (int i = 0; i < N; ++i) {
+            if (i > 0 && A[i] > A[i - 1]) up[i] = up[i - 1] + 1;
+            if (up[i] && down[i]) res = max(res, up[i] + down[i] + 1);
+        }
+        return res;
+    }
+};
