f3e7dc2eff61814965fa0a230c1741f43331e50b
Aman Shankar singh <96378145+singhAman05@users.noreply.github.com>
2024-02-17 17:19:48 +0530

    Update Furthest Building You Can Reach.cpp

--- Furthest Building You Can Reach.cpp
+++ Furthest Building You Can Reach.cpp
@@ -1,5 +1,6 @@
 /*
   This question can be the classic DP question so I have discussed all the three approach here
+  In this question I have used LAZY GREEDY so do check this approach
 */
 
 // <---------- Aproach 1 -------------->
@@ -28,6 +29,35 @@ public:
     }
     int furthestBuilding(vector<int>& heights, int bricks, int ladders) {
         n = heights.size();
+        return solve(heights,bricks,ladders,0);
+    }
+};
+
+
+// <-------------Approach 2----------------->
+// memory limit exceeded
+// recursion + memization
+
+class Solution {
+public:
+    int n;
+    int dp[100001][1000000001];
+    int solve(vector<int>& heights, int bricks, int ladders, int ind){
+        if(ind==n-1) return dp[bricks][ladders] = ind;
+        if(dp[bricks][ladders]!=-1) return dp[bricks][ladders];
+
+        if(heights[ind]>=heights[ind+1]){
+            return solve(heights,bricks,ladders,ind+1);
+        }
+        int br=ind,lad=ind;
+        if((heights[ind+1]-heights[ind]) <= bricks) br = solve(heights,bricks-(heights[ind+1]-heights[ind]),ladders,ind+1);
+        if(ladders>0) lad = solve(heights,bricks,ladders-1,ind+1);
+
+        return  dp[bricks][ladders] = max({br,lad});
+    }
+    int furthestBuilding(vector<int>& heights, int bricks, int ladders) {
+        n = heights.size();
+        memset(dp,-1,sizeof(dp));
         return solve(heights,bricks,ladders,0);
     }
 };
