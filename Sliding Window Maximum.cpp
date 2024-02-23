380122d0a1760335b44d43546551a0092e110c5a
Aman Shankar singh <96378145+singhAman05@users.noreply.github.com>
2024-02-23 21:27:50 +0530

    Update Sliding Window Maximum.cpp

--- Sliding Window Maximum.cpp
+++ Sliding Window Maximum.cpp
@@ -19,3 +19,37 @@ public:
         return ans;
     }
 };
+
+
+/*
+   In this solution we will just kee track of maximum element in a deque and popout if it reaches out of bound
+*/
+
+// TCO(N)
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        int n = nums.size();
+        vector<int> ans;
+
+        deque<int> dq;
+        for(int i=0;i<n;i++){
+
+            // poping out those element which are out of the window
+            while(!dq.empty() && dq.front()<=i-k) dq.pop_front();
+            
+            // poping out the elements which are smaller than the current element
+            while(!dq.empty() && nums[i] > nums[dq.back()]) dq.pop_back();
+
+            // pushing the current index
+            dq.push_back(i);
+
+            // adding front element in window
+            if(i-k+1 >= 0){
+                ans.push_back(nums[dq.front()]);
+            }
+        }
+
+        return ans;
+    }
+};
